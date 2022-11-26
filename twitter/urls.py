8c8217e45c62a74aebf1111a12744a74d61f94df
Weilong <byegates@gmail.com>
2022-11-25 19:22:42 -0800

    11-comments-create-api

--- twitter/urls.py
+++ twitter/urls.py
@@ -18,9 +18,10 @@ from django.urls import include, path
 from rest_framework import routers
 
 from accounts.api.views import UserViewSet, AccountViewSet
-from tweets.api.views import TweetViewSet
+from comments.api.views import CommentViewSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
+from tweets.api.views import TweetViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
@@ -28,6 +29,7 @@ router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
+router.register(r'api/comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
