99eaea56c5344420b19e45fb4b0c730269ba8cd3
Weilong <byegates@gmail.com>
2022-11-27 11:38:21 -0800

    15-likes-model-and-admin

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from likes.models import Like
 from utils.time_helpers import utc_now
-
 
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -12,11 +13,18 @@ class Tweet(models.Model):
         index_together = (('user', 'created_at'),)
         ordering = ('user', '-created_at')
 
+    def __str__(self):
+        # 这里是你执行 print(tweet instance) 的时候会显示的内容
+        return f'{self.created_at} {self.user}: {self.content}'
+
     @property
     def hours_to_now(self):
         # datetime.now 不带时区信息，需要增加上 utc 的时区信息
         return (utc_now() - self.created_at).seconds // 3600
 
-    def __str__(self):
-        # 这里是你执行 print(tweet instance) 的时候会显示的内容
-        return f'{self.created_at} {self.user}: {self.content}'
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Tweet),
+            object_id=self.id,
+        ).order_by('-created_at')
