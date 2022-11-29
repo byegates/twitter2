d0e143b74f13f620ceeb238d0be50913f1fb6d2f
Weilong <byegates@gmail.com>
2022-11-29 03:12:57 -0800

    34-cache-newsfeed-list-in-redis

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -1,25 +1,20 @@
 from newsfeeds.api.serializers import NewsFeedSerializer
 from newsfeeds.models import NewsFeed
+from newsfeeds.services import NewsFeedService
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from utils.paginations import EndlessPagination
 
-
 class NewsFeedViewSet(viewsets.GenericViewSet):
+    queryset = NewsFeed.objects.all() # Added to avoid newsfeeds api failure
     permission_classes = [IsAuthenticated]
     pagination_class = EndlessPagination
 
-    def get_queryset(self):
-        # 自定义 queryset，因为 newsfeed 的查看是有权限的
-        # 只能看 user=当前登录用户的 newsfeed
-        # 也可以是 self.request.user.newsfeed_set.all()
-        # 但是一般最好还是按照 NewsFeed.objects.filter 的方式写，更清晰直观
-        return NewsFeed.objects.filter(user=self.request.user)
-
     def list(self, request):
-        queryset = self.paginate_queryset(self.get_queryset())
+        newsfeeds = NewsFeedService.get_cached_newsfeeds(request.user.id)
+        page = self.paginate_queryset(newsfeeds)
         serializer = NewsFeedSerializer(
-            queryset,
+            page,
             context={'request': request},
             many=True,
         )
